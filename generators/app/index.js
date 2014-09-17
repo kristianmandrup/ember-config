c7b72304b6cc609f4af7859c2400efefba5bccff
Kristian Mandrup <kmandrup@gmail.com>
2014-09-17 14:48:40 +0200

    addons on app gen

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,7 @@ var helper = require ('../../lib/aid');
 var aid;
 var selected;
 
-var availableConfigs = ['script', 'css', 'templating', 'layout', 'test', 'adapters', 'mobile', 'fonts', 'components', 'auth', 'libs'];
+var availableConfigs = ['script', 'css', 'templating', 'layout', 'test', 'adapters', 'mobile', 'fonts', 'components', 'auth', 'addons', 'libs'];
 
 var endScripts = {};
 availableConfigs.forEach(function(config) {
