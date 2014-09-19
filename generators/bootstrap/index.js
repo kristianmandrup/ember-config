01782daa38fbaeac5158133ea1048d7359e82bd3
Kristian Mandrup <kmandrup@gmail.com>
2014-09-19 15:26:12 +0200

    improve bootstrap prompts

--- generators/bootstrap/index.js
+++ generators/bootstrap/index.js
@@ -21,30 +21,46 @@ var EmberConfigBootstrapGenerator = yeoman.generators.Base.extend({
   },
 
   // Choose test framework
-  prompting: function () {
-    var done = this.async();
+  prompting: {
+    features: function () {
+      var done = this.async();
 
-    // TODO: should detect app is using sass and change default!
-    var prompts = [{
-      type: 'list',
-      name: 'cssType',
-      message: 'Which styling language for bootstrap would you like?',
-      choices: ['less', 'sass'],
-      default: 'css'
-    }, {
-      type: 'checkbox',
-      name: 'features',
-      message: 'Which features of Twitter bootstrap would you like?',
-      choices: ['css', 'javascript', 'fonts'],
-      default: ['css', 'javascript']
-    }];
+      // TODO: should detect app is using sass and change default!
+      var prompts = [{
+        type: 'checkbox',
+        name: 'features',
+        message: 'Which features of Twitter bootstrap would you like?',
+        choices: ['stylesheets', 'javascript', 'fonts'],
+        default: ['stylesheets', 'javascript']
+      }];
 
-    this.prompt(prompts, function (props) {
-      this.cssType = props.cssType;
-      this.features = props.features;
+      this.prompt(prompts, function (props) {
+        this.features = props.features;
 
-      done();
-    }.bind(this));
+        done();
+      }.bind(this));
+    },
+
+    stylesheets: function () {
+      if (!selected('stylesheets')) return;
+
+      var done = this.async();
+
+      // TODO: should detect app is using sass and change default!
+      var prompts = [{
+        type: 'list',
+        name: 'cssType',
+        message: 'Which styling language for bootstrap would you like?',
+        choices: ['css', 'sass'],
+        default: 'css'
+      }];
+
+      this.prompt(prompts, function (props) {
+        this.cssType = props.cssType;
+
+        done();
+      }.bind(this));
+    }    
   },
 
   writing: {
@@ -54,26 +70,26 @@ var EmberConfigBootstrapGenerator = yeoman.generators.Base.extend({
       // http://www.octolabs.com/blogs/octoblog/2014/05/10/ember-cli-broccoli-bootstrap-sass-part-2/
       var sassFileContent = aid.fileContent('app/styles/app.scss');
 
-      var import_sass = "@import '" + this.bowerDir + "/bootstrap-sass-official/assets/stylesheets/bootstrap'";
-      if (sassFileContent.has(import_sass)) return;
+      var importSass = "@import '" + this.bowerDir + "/bootstrap-sass-official/assets/stylesheets/bootstrap'";
+      if (sassFileContent.has(importSass)) return;
 
       sass_file.app(function() {
-        this.prependTxt(import_sass);
+        this.prependTxt(importSass);
       });
-      aid.info('Bootstrap for SASS configured');
+      aid.info('Bootstrap SASS configured');
     },
     configureCss: function () {                
       if (cssSelected('sass')) return;
 
-      var css_import = "app.import('" + this.bowerDir + "/bootstrap/dist/css/bootstrap.css');";
+      var cssImport = "app.import('" + this.bowerDir + "/bootstrap/dist/css/bootstrap.css');";
 
-      if (this.brocFileContent.has(css_import)) return;
+      if (this.brocFileContent.has(cssImport)) return;
 
       broc_file(function() {
-        return this.last('module.exports').prepend(css_import + '\n');
+        return this.last('module.exports').prepend(cssImport + '\n');
       }).write();
 
-      aid.info('Bootstrap for CSS configured');
+      aid.info('Bootstrap CSS configured');
     },
 
     configureJs: function () {  
